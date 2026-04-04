b9dd8656543e295516d3f81beb990e301ef64fd6
Anurag singh <anusingh99361634@gmail.com>
2026-04-04 18:27:44 +0530

    create Home-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,11 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import Homepage from './landing_page/home/HomePage'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Hello</h1>
+      <Homepage/>
     </>
   )
 }
---
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function OpenAccount() {
+    return ( 
+        <h1>openaccount</h1>
+     );
+}
+
+export default OpenAccount;
