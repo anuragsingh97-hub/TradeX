fbf62b0924cd2406d2d2b42ef2f26b0cec0cee40
Anurag singh <anusingh99361634@gmail.com>
2026-04-05 13:34:30 +0530

    footer Navbar

--- frontend/src/landing_page/Navbar.jsx
+++ frontend/src/landing_page/Navbar.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-      <div class="container-fluid p-2">
+    <nav
+      class="navbar navbar-expand-lg border-bottom"
+      style={{ backgroundColor: "#FFF" }}
+    >
+      <div class="container p-2">
         <a class="navbar-brand" href="#">
           <img
             src="media/images/logo.svg"
-            alt="logo"
             style={{ width: "25%" }}
+            alt="Logo"
           />
         </a>
         <button
@@ -24,30 +27,30 @@ function Navbar() {
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <form class="d-flex" role="search">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul class="navbar-nav mb-lg-0">
               <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="#">
                   Signup
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link active" href="#" >
-                About
-                </a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="#">
-                Product
+                <a class="nav-link active" href="#">
+                  About
                 </a>
               </li>
               <li class="nav-item">
                 <a class="nav-link active" href="#">
-                Pricing
+                  Product
                 </a>
               </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="#">
-                Support
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" href="#">
+                  Support
                 </a>
               </li>
             </ul>
--- frontend/src/landing_page/OpenAccount.jsx
+++ frontend/src/landing_page/OpenAccount.jsx
@@ -1,28 +1,23 @@
-import React from 'react';
+import React from "react";
 
 function OpenAccount() {
-    return ( 
-        <div className="container p-5 mb-5">
+  return (
+    <div className="container p-5 mb-5">
       <div className="row text-center">
-        <img
-          src="media/images/homeHero.png"
-          alt="Hero Image"
-          className="mb-5"
-        />
-        <h1 className="mt-5">Invest in everything</h1>
+        <h1 className="mt-5">Open a Zerodha account</h1>
         <p>
-          Online platform to invest in stocks, derivatives, mutual funds, and
-          more
+          Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
+          F&O trades.
         </p>
         <button
           className="p-2 btn btn-primary fs-5 mb-5"
           style={{ width: "20%", margin: "0 auto" }}
         >
-          Signup Now
+          Sign up Now
         </button>
       </div>
     </div>
-     );
+  );
 }
 
 export default OpenAccount;
