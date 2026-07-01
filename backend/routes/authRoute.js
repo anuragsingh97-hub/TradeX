7c22ad09790cb083b143f6930419d371b028cd42
Anurag singh <anusingh99361634@gmail.com>
2026-07-01 21:11:18 +0530

    change autontroller.js to AuthController.js

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const authController = require("../controllers/autontroller");
+const authController = require("../controllers/authController");
 
 router.post("/signup", authController.signup);
 
