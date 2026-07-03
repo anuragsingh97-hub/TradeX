a31420f3e8f265bcdecf1db7ef67b513b5ddb8e8
Anurag singh <anusingh99361634@gmail.com>
2026-07-04 01:20:44 +0530

    API add

--- dashboard/src/api/api.js
+++ dashboard/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "https://zerotrade-eidw.onrender.com",
- baseURL: "http://localhost:3002",
+  baseURL: "https://zerotrade-eidw.onrender.com",
+  //  baseURL: "http://localhost:3002",
   withCredentials: true,
 });
 
